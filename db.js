78d1c5402dadbca6379e29a46b72f80deff9469c
Denny <dennyrustandi1094@gmail.com>
2019-05-06 19:39:01 +0700

    Add console log

--- db.js
+++ db.js
@@ -2,6 +2,8 @@ var Sequelize = require('sequelize');
 var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
+console.log(env, process.env.DATABASE_URL);
+
 if (env === 'production') {
     sequelize = new Sequelize(process.env.DATABASE_URL, {
         dialect: 'postgres'
